12674cf5b7587f30bdf3527cfba8dec755b15e80
meads58 <meads109@gmail.com>
2015-03-24 18:09:54 +0000

    images loading and paths set up with app.use.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static(__dirname + "/public"));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
