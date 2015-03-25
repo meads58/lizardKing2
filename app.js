460cc900a10aebf9856defeb105b35c73cf7fc6d
Edward Brown <edward.brown.london@gmail.com>
2015-03-25 11:03:26 +0000

    removed erroneous host in server

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.listen(port, function () {
  // var host = server.address().address
  // var port = server.address().port
 
- console.log('App hopefully running at http://%s:%s', host, port);
+ console.log('App hopefully running at http://%s:%s', port);
 
 })
