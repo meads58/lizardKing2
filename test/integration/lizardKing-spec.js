4fd7af08c931fe4dcaf269b6d743621a4e918c4c
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-25 10:42:17 +0000

    tidied syntax

--- test/integration/lizardKing-spec.js
+++ test/integration/lizardKing-spec.js
@@ -2,15 +2,17 @@ describe('lizard King', function() {
 
   beforeEach(function() {
     browser.get("http://localhost:3000");
-  })
+  });
 
   describe('title', function() {
 
     it('has the title lizard king.', function() {
       expect(browser.getTitle()).toEqual("Lizard King");
-    })
+    });
 
-  })
+
+
+  });
 
 
 });
