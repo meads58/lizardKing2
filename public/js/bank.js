1c6955234d3ec57a97d8a2df01b5118c3cf4d6bc
Diego <diegoromero.audio@gmail.com>
2015-03-26 19:13:09 +0000

    fixed some styling issues and added header

--- public/js/bank.js
+++ public/js/bank.js
@@ -1,18 +1,54 @@
 var bank = [
-  {
-    url: "https://thechive.files.wordpress.com/2008/12/funny-hilarious-bird-pics-pictures12.jpg?quality=94&strip=all",
-    count: 0
-  },
-  {
-    url: "http://infusionsoftva.com/wp-content/uploads/2014/09/hilarious-pictures.jpg",
-    count: 0
-  },
-  {
-    url: "http://r.fod4.com/http://p.fod4.com/p/media/c81a6839b7/NSaD49Y5TVC5VSdXiGiM_f1.jpg",
-    count: 0
-  },
-  {
-    url: "http://www.pictures1.org/wp-content/uploads/2012/06/funny-Photos.jpg",
-    count: 0
-  }
+ {
+   url: "https://thechive.files.wordpress.com/2008/12/funny-hilarious-bird-pics-pictures12.jpg?quality=94&strip=all",
+   count: 0
+ },
+ {
+   url: "http://cdn.funnyhub.com/2015/feb/bas/Funny-Pictures/funny-middle-finger-old-lady.gif",
+   count: 0
+ },
+ {
+   url: "http://r.fod4.com/http://p.fod4.com/p/media/c81a6839b7/NSaD49Y5TVC5VSdXiGiM_f1.jpg",
+   count: 0
+ },
+ {
+   url: "http://quicklol.com/wp-content/uploads/2015/03/xmen-toilet.jpg",
+   count: 0
+ },
+ {
+   url: "http://quicklol.com/wp-content/uploads/2015/03/american-dealer.jpg",
+   count: 0
+ },
+ {
+   url: "http://quicklol.com/wp-content/uploads/2015/02/brave-girl.jpg",
+   count: 0
+ },
+ {
+   url: "http://quicklol.com/wp-content/uploads/2015/02/keep-scrolling-cat-with-knife.jpg",
+   count: 0
+ },
+ {
+   url: "http://quicklol.com/wp-content/uploads/2015/02/walking-into-a-glass-door.gif",
+   count: 0
+ },
+ {
+   url: "http://quicklol.com/wp-content/uploads/2015/02/destroy-uranus.jpg",
+   count: 0
+ },
+ {
+   url: "https://i.chzbgr.com/maxW500/7991192576/h63F9A9A7/",
+   count: 0
+ },
+ {
+   url: "https://i.chzbgr.com/maxW500/7960523008/h11171374/",
+   count: 0
+ },
+ {
+   url: "http://quicklol.com/wp-content/uploads/2013/08/a-good-book.jpg",
+   count: 0
+ },
+ {
+   url: "https://i.chzbgr.com/maxW500/7910003712/hF3CC0362/",
+   count: 0
+ }
 ];
