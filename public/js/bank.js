c8a028b5662f87d2e2c8ca83ab0cb8d2dfaace3c
Edward Brown <edward.brown.london@gmail.com>
2015-03-26 15:48:41 +0000

    outsourced bank

---
+++ public/js/bank.js
@@ -0,0 +1,18 @@
+var bank = [
+  {
+    url: "https://thechive.files.wordpress.com/2008/12/funny-hilarious-bird-pics-pictures12.jpg?quality=94&strip=all",
+    count: 0
+  },
+  {
+    url: "http://infusionsoftva.com/wp-content/uploads/2014/09/hilarious-pictures.jpg",
+    count: 0
+  },
+  {
+    url: "http://r.fod4.com/http://p.fod4.com/p/media/c81a6839b7/NSaD49Y5TVC5VSdXiGiM_f1.jpg",
+    count: 0
+  },
+  {
+    url: "http://www.pictures1.org/wp-content/uploads/2012/06/funny-Photos.jpg",
+    count: 0
+  }
+];
